807152cfde6b42a6f452e62c2c64209ceea28dae
Joshua Hendrix <joshuathendrix@gmail.com>
2019-01-05 17:13:07 -0700

    files

---
+++ script.js
@@ -0,0 +1,37 @@
+  $(document).ready(function(){
+      $("li").hover(function(){
+          $(this).css("font-size", "29px");
+    }, function(){
+          $(this).css("font-size", "25px");
+    });
+  });
+
+  function loadRepo(url, callback) {
+    const gitRequest = new XMLHttpRequest();
+  
+    gitRequest.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        callback(this);
+      }
+      
+    };
+    gitRequest.open("GET", url, true);
+    gitRequest.send();
+  }
+  
+  function loadRepoCallback(gitRequest) {
+    var htmlUpdate = "";
+    let gitRepo = JSON.parse(gitRequest.responseText);
+    gitRepo.forEach(function(inRepo) {
+      htmlUpdate =
+        htmlUpdate +
+        "<li> " +
+        '<a href="' +
+        inRepo.html_url +
+        '" target="_blank">' +
+        inRepo.name +
+        "</a></li> ";
+    });
+    document.getElementById("repoList").innerHTML = htmlUpdate;
+  }
+  
